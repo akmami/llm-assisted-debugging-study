4cb35955c3820704fb2c1be5d8e9e0ff624beeb7
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2026-05-03 01:34:24 +0300

    guideline added for latest setup

---
+++ GUIDELINE.sh
@@ -0,0 +1,47 @@
+# Participant Guidelines
+
+Thank you for participating in this study on debugging and AI-assisted feedback.
+
+This guideline explains how to complete the tasks and submit your solutions.
+
+---
+
+## Setup
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/akmami/llm-assisted-debugging-study.git
+cd llm-assisted-debugging-study/latest
+```
+
+1. Prepare study package:
+
+```bash
+source 12-prepare-final.sh
+```
+
+## Task Overview
+
+
+Your goal is to go over the [Google Forms](https://forms.gle/y2f9nN3J89nzsTDa7) and try to answer the questions based on the given files. The patches cases can be viewed from 'Source Control' section. For this, you will need VS Code.
+
+### Notes
+
+You are encouraged to rely on your own reasoning
+
+Please do not go back in survey and change the answers
+
+### Time Expectation
+
+The full set of tasks is designed to be completed within a reasonable time frame (less than 60 minutes). You may proceed at your own pace unless instructed otherwise.
+
+### Questions
+
+If anything is unclear, please contact the researcher before proceeding.
+
+## Survey
+
+Once the debugging session is completed, we kindly ask you to take a survey using [Google Forms](https://forms.gle/4kVttrjqoKmq9dZR7)
+
+Thanks again for your participation!
