60ea04580f4058662b7871e501a284235077689c
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2026-05-02 23:56:47 +0300

    final preperation completed ...

---
+++ latest/12-prepare-final.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+rm -rf participant-package
+
+cp buggy_codes/* participant-package
+
+git add participant-package
+
+cp patched_codes/* participant-package
+
+echo "Please look for patch diff from 'Source Control' section ..." 
